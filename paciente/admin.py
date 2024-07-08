8658a80946b4cbdc7ea5e2b0ecd8fe47854bdcab
Alexandre Santos <111398880+allesantos@users.noreply.github.com>
2024-07-08 00:25:44 -0300

    update

---
+++ paciente/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Consulta, Documento
+
+admin.site.register(Consulta)
+admin.site.register(Documento)
+
+
---
+++ usuarios/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib import auth
+
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        users = User.objects.filter(username=username)
+
+        if users.exists():
+            print('Erro 1')
+            return redirect('/usuarios/cadastro')
+
+        if senha != confirmar_senha:
+            print('Erro ')
+            return redirect('/usuarios/cadastro')
+
+        if len(senha) < 8:
+            print('Erro 3')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=senha
+            )
+            return redirect('/usuarios/login')
+        except:
+            print('Erro 4')
+            return redirect('/usuarios/cadastro')
+ 
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get("senha")
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
+        return redirect('/usuarios/login')
+    
+def sair(request):
+    auth.logout(request)
+    return redirect('/usuarios/login')
